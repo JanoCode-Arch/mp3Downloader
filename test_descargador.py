16871c0935359810580ae88a326d87b44b8a63f8
JanoCode-Arch <avla200026@gmail.com>
2026-06-27 16:21:08 -0600

    Agrega test de construir_opciones

--- test_descargador.py
+++ test_descargador.py
@@ -1,5 +1,6 @@
 import pytest
 from descargador import validar
+from descargador import construir_opciones
 
 def test_validar_url_vacia_lanza_error():
     with pytest.raises(ValueError):
@@ -7,3 +8,13 @@ def test_validar_url_vacia_lanza_error():
 
 def test_validar_todo_lleno_no_lanza():
     validar("https://www.youtube.com/watch?v=IT_dC-ziqs0", "Lumare-Tutto-Passa", "C:/Users/avla2/Music/Testing")
+    
+def test_construir_opciones_arma_ruta_correc():
+    opts = construir_opciones("cancion","C:/musica",None)
+    assert opts["outtmpl"] == "C:/musica/cancion.%(ext)s"
+
+def test_construir_opciones_es_mp3():
+    opts = construir_opciones("c", "d", None)
+    post = opts["postprocessors"][0]
+    assert post["preferredcodec"] == "mp3"
+    assert post["preferredquality"] == "0"
